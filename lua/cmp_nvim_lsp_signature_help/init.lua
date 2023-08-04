49308e60cc717ff6f76247a00ceb599219ae1c62
IsaacShelton <isaac.a.shelton@gmail.com>
2023-08-04 09:33:45 -0500

    Attempt fix issue

--- lua/cmp_nvim_lsp_signature_help/init.lua
+++ lua/cmp_nvim_lsp_signature_help/init.lua
@@ -116,7 +116,7 @@ source._item = function(self, signature, parameter_index)
 
   local label = table.concat(arguments, ', ')
   return {
-    label = label,
+    label = string.gsub(label, "_", "\\_"),
     filterText = ' ',
     insertText = self:_matchstr(label, [[\k\+]]),
     word = '',
@@ -169,7 +169,7 @@ source._signature_label = function(self, signature, parameter_index)
     local s, e = string.find(label, self:_parameter_label(signature, signature.parameters[parameter_index]), 1, true)
     if s and e then
       local active = string.sub(label, s, e)
-      label = string.gsub(string.gsub(label, "_", "\\_"), vim.pesc(active), '***' .. active .. '***')
+      label = string.gsub(label, vim.pesc(active), '***' .. active .. '***')
     end
   end
   return label
